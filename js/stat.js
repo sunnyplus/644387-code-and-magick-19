715b4e1abf1bc5c7907884a9830a1b007c7141f1
alexey <sunnyplus@mail.ru>
2020-02-05 09:56:30 +0300

    правки 2-1

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,11 @@
 'use strict';
 
+var START_X = 0;
+var START_Y = 0;
 var BAR_START_X = 130;
 var BAR_START_Y = 235;
 var CLOUD_START_X = 100;
 var CLOUD_START_Y = 10;
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
 var BAR_GAP = 50;
 var BAR_WIDTH = 40;
 var TEXT_GAP = 15;
@@ -24,18 +24,66 @@ var renderCurve = function (ctx, x1, y1, x2, y2) {
   ctx.quadraticCurveTo(x2, y2, x1, y1);
 };
 
+var cloudArray = [
+  {
+    curveToX: 200,
+    curveToY: 30,
+    controlX: 150,
+    controlY: 10
+  },
+  {
+    curveToX: 400,
+    curveToY: 30,
+    controlX: 300,
+    controlY: 0
+  },
+  {
+    curveToX: 480,
+    curveToY: 50,
+    controlX: 430,
+    controlY: 10
+  },
+  {
+    curveToX: 480,
+    curveToY: 250,
+    controlX: 520,
+    controlY: 160
+  },
+  {
+    curveToX: 400,
+    curveToY: 280,
+    controlX: 420,
+    controlY: 300
+  },
+  {
+    curveToX: 200,
+    curveToY: 280,
+    controlX: 300,
+    controlY: 300
+  },
+  {
+    curveToX: 100,
+    curveToY: 220,
+    controlX: 120,
+    controlY: 280
+  },
+  {
+    curveToX: 100,
+    curveToY: 50,
+    controlX: 100,
+    controlY: 100
+  }
+];
+
 var renderCloud = function (ctx, fillColor, shiftX, shiftY) {
   ctx.beginPath();
   ctx.fillStyle = fillColor;
   ctx.moveTo(CLOUD_START_X + shiftX, CLOUD_START_Y + shiftY);
-  renderCurve(ctx, 200 + shiftX / 2, 30 + shiftY / 2, 150 + shiftX, 10 + shiftY);
-  renderCurve(ctx, 400 + shiftX / 2, 30 + shiftY / 2, 300 + shiftX, 0 + shiftY);
-  renderCurve(ctx, 480 + shiftX / 2, 50 + shiftY / 2, 430 + shiftX, 10 + shiftY);
-  renderCurve(ctx, 480 + shiftX / 2, 250 + shiftY / 2, CLOUD_START_X + CLOUD_WIDTH + shiftX, 160 + shiftY);
-  renderCurve(ctx, 400 + shiftX / 2, CLOUD_HEIGHT + CLOUD_START_Y + shiftY / 2, 420 + shiftX, 300 + shiftY);
-  renderCurve(ctx, 200 + shiftX / 2, CLOUD_HEIGHT + CLOUD_START_Y + shiftY / 2, 300 + shiftX, 300 + shiftY);
-  renderCurve(ctx, 100 + shiftX / 2, 220 + shiftY / 2, 120 + shiftX, 280 + shiftY);
-  renderCurve(ctx, 100 + shiftX / 2, 50 + shiftY / 2, 0, 100 + shiftX, 100 + shiftY);
+
+  cloudArray.forEach(function (elm) {
+    renderCurve(ctx, elm.curveToX + shiftX, elm.curveToY + shiftY, elm.controlX + shiftX, elm.controlY + shiftY);
+  });
+
   ctx.fill();
   ctx.closePath();
 };
@@ -44,7 +92,7 @@ window.renderStatistics = function (ctx, players, times) {
 
   renderCloud(ctx, 'rgba(0, 0, 0, 0.7)', SHADOW_X_GAP, SHADOW_Y_GAP);
   ctx.font = '16 px PT Mono';
-  renderCloud(ctx, '#fff', 0, 0);
+  renderCloud(ctx, '#fff', START_X, START_Y);
   ctx.fillStyle = '#000';
   ctx.fillText('Ура вы победили!', BAR_START_X + TITLE_X_GAP, CLOUD_START_Y + TITLE_Y_GAP);
   ctx.fillText('Список результатов:', BAR_START_X + TITLE_X_GAP, CLOUD_START_Y + TITLE_Y_GAP + RESULTS_Y_GAP);
@@ -70,3 +118,5 @@ window.renderStatistics = function (ctx, players, times) {
     ctx.fillText(Math.floor(times[x]), BAR_START_X + g, BAR_START_Y - currentHeight - FIGURES_GAP);
   }
 };
+
+
